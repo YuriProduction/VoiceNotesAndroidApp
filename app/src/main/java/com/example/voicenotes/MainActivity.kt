870526cef1cb67e2d1b580f6ee8e7f5597d43ecb
Jurex <jurexda@yandex.ru>
2023-10-16 19:10:35 +0500

    Try to draw the main activity.

--- app/src/main/java/com/example/voicenotes/MainActivity.kt
+++ app/src/main/java/com/example/voicenotes/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.voicenotes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolBar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        val buttonClick = findViewById<Button>(R.id.button_click)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item -> {
+                // Обработка нажатия на иконку бургера (меню)
+                // Здесь вы можете открыть ваше меню или выполнить другие действия
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }
