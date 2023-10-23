7f7c6417585f1f2bad9d42636c1a0904fefccb6e
Anton Volkov <xzxz2020.ru@mail.ru>
2023-10-24 00:07:18 +0500

    Добавлен фронтенд бокового меню, кнопок, диалогового окна, фрейма папки

--- app/src/main/java/com/example/voicenotes/MainActivity.kt
+++ app/src/main/java/com/example/voicenotes/MainActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputType
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,23 +49,75 @@ class MainActivity : AppCompatActivity() {
             builder.
             create().show()
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Название папки"
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.menu_item -> {
-                // Обработка нажатия на иконку бургера (меню)
-                // Здесь вы можете открыть ваше меню или выполнить другие действия
+            R.id.nav_root_folder -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, FolderNotesFragment()).commit()
                 return true
             }
+            R.id.nav_folder1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, FolderNotesFragment()).commit()
+                return true
+            }
+            R.id.nav_folder2 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, FolderNotesFragment()).commit()
+                return true
+            }
+            R.id.nav_add_folder -> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Введите название папки")
 
-            else -> return super.onOptionsItemSelected(item)
+                // Создать поле ввода текста
+                val input = EditText(this)
+                input.inputType = InputType.TYPE_CLASS_TEXT
+                input.filters = arrayOf(InputFilter.LengthFilter(40)) // Максимальная длина 40 символов
+                builder.setView(input)
+
+                // Установить кнопку "Отмена"
+                builder.setNegativeButton("Отмена") { dialog, _ ->
+                    dialog.cancel()
+                }
+
+                // Установить кнопку "Ок"
+                builder.setPositiveButton("Ок") { dialog, _ ->
+                    val enteredText = input.text.toString()
+                    dialog.dismiss()
+                    val intent = Intent(this, VoiceActivity::class.java)
+                    // Передать данные, если необходимо
+                    intent.putExtra("folderTitle", enteredText)
+                }
+
+                // Показать диалоговое окно
+                builder.
+                create().show()
+
+                return true
+            }
+            R.id.nav_cart -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, FolderNotesFragment()).commit()
+                return true
+            }
         }
+        return false
     }
 
 }
