c41e5728e0e84311c479a69202dc4708509377f1
Jurex <jurexda@yandex.ru>
2023-10-24 10:29:22 +0500

    empty commit!

--- app/src/main/java/com/example/voicenotes/VoiceActivity.kt
+++ app/src/main/java/com/example/voicenotes/VoiceActivity.kt
@@ -1,13 +1,25 @@
 package com.example.voicenotes
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import org.romancha.playpause.PlayPauseView
 
 class VoiceActivity : AppCompatActivity() {
+    private val RQS_RECORDING = 1
+    private var savedUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.voice_activity)
-
+        val playPauseView = findViewById<PlayPauseView>(R.id.play_pause_view)
+        playPauseView.setOnClickListener {
+            // View clicked
+            playPauseView.toggle()
+        }
     }
+
 }
