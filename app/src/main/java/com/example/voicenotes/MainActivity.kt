0bf850b4fe5e29c725980b85981f835e1740ae35
Jurex <jurexda@yandex.ru>
2023-11-03 23:14:45 +0500

    Dynamically adding items.

--- app/src/main/java/com/example/voicenotes/MainActivity.kt
+++ app/src/main/java/com/example/voicenotes/MainActivity.kt
@@ -1,53 +1,38 @@
 package com.example.voicenotes
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputType
+import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView.Orientation
 import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var mainLayout: LinearLayout // Reference to your main vertical LinearLayout
+    private val REQUEST_CODE_VOICE_ACTIVITY = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainLayout = findViewById(R.id.fragment_container)
+
         val buttonClick = findViewById<Button>(R.id.button_click)
         buttonClick.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            // Установить заголовок (необязательно)
-            builder.setTitle("Введите название заметки")
-            // Создать поле ввода текста
-            val input = EditText(this)
-            input.inputType = InputType.TYPE_CLASS_TEXT
-            input.filters = arrayOf(InputFilter.LengthFilter(40))
-            builder.setView(input)
-            // Установить кнопку "Отмена"
-            builder.setNegativeButton("Отмена") { dialog, _ ->
-                dialog.cancel()
-            }
-
-            // Установить кнопку "Ок"
-            builder.setPositiveButton("Ок") { dialog, _ ->
-                val enteredText = input.text.toString()
-                dialog.dismiss()
-                val intent = Intent(this, VoiceActivity::class.java)
-                // Передать данные, если необходимо
-                intent.putExtra("noteTitle", enteredText)
-                // Запустить новую Activity
-                startActivity(intent)
-                // Здесь вы можете использовать введенный текст (enteredText) по вашему усмотрению
-            }
-            // Показать диалоговое окно
-            builder.
-            create().show()
+            val intent = Intent(this, VoiceActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_VOICE_ACTIVITY)
         }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
@@ -64,6 +49,59 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_VOICE_ACTIVITY && resultCode == RESULT_OK) {
+            val newItemText = data?.getStringExtra("new_item_text")
+            val noteItemContent = LinearLayout(this)
+            noteItemContent.orientation = LinearLayout.HORIZONTAL
+            noteItemContent.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            // Create a button on the left
+            val leftButton = Button(this)
+            leftButton.text = "▶️";
+            leftButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                0f
+            )
+            leftButton.gravity = Gravity.START
+            leftButton.setBackgroundColor(Color.WHITE)
+
+            // Create a button on the right
+            val rightButton = Button(this)
+            rightButton.text = "❌"
+            rightButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                0f
+            )
+            rightButton.gravity = Gravity.END
+            rightButton.setBackgroundColor(Color.WHITE)
+
+            // Create the title in the middle
+            val titleTextView = TextView(this)
+            titleTextView.text = newItemText
+            titleTextView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                1f
+            )
+            titleTextView.setTextColor(Color.BLACK)
+            titleTextView.gravity = Gravity.CENTER
+
+            // Add the views to the horizontal layout
+            noteItemContent.addView(leftButton)
+            noteItemContent.addView(titleTextView)
+            noteItemContent.addView(rightButton)
+            // Add the horizontal layout to the vertical layout
+            mainLayout.addView(noteItemContent)
+        }
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_root_folder -> {
@@ -71,16 +109,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     .replace(R.id.fragment_container, FolderNotesFragment()).commit()
                 return true
             }
+
             R.id.nav_folder1 -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, FolderNotesFragment()).commit()
                 return true
             }
+
             R.id.nav_folder2 -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, FolderNotesFragment()).commit()
                 return true
             }
+
             R.id.nav_add_folder -> {
                 val builder = AlertDialog.Builder(this)
                 builder.setTitle("Введите название папки")
@@ -88,7 +129,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 // Создать поле ввода текста
                 val input = EditText(this)
                 input.inputType = InputType.TYPE_CLASS_TEXT
-                input.filters = arrayOf(InputFilter.LengthFilter(40)) // Максимальная длина 40 символов
+                input.filters =
+                    arrayOf(InputFilter.LengthFilter(40)) // Максимальная длина 40 символов
                 builder.setView(input)
 
                 // Установить кнопку "Отмена"
@@ -106,11 +148,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 }
 
                 // Показать диалоговое окно
-                builder.
-                create().show()
+                builder.create().show()
 
                 return true
             }
+
             R.id.nav_cart -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, FolderNotesFragment()).commit()
--- app/src/main/java/com/example/voicenotes/VoiceActivity.kt
+++ app/src/main/java/com/example/voicenotes/VoiceActivity.kt
@@ -5,6 +5,11 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.InputFilter
+import android.text.InputType
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import org.romancha.playpause.PlayPauseView
 
@@ -22,7 +27,32 @@ class VoiceActivity : AppCompatActivity() {
             }
             else
             {
-                //TODO - Реализовать логику добавления в ФС аудиозаписи
+                val builder = AlertDialog.Builder(this)
+                // Установить заголовок (необязательно)
+                builder.setTitle("Введите название заметки")
+                // Создать поле ввода текста
+                val input = EditText(this)
+                input.inputType = InputType.TYPE_CLASS_TEXT
+                input.filters = arrayOf(InputFilter.LengthFilter(40))
+                builder.setView(input)
+                // Установить кнопку "Отмена"
+                builder.setNegativeButton("Отмена") { dialog, _ ->
+                    dialog.cancel()
+                }
+
+                // Установить кнопку "Ок"
+                builder.setPositiveButton("Ок") { dialog, _ ->
+                    val enteredText = input.text.toString()
+                    val newItemText = enteredText // Replace with the actual item text you want to add
+                    val intent = Intent()
+                    intent.putExtra("new_item_text", newItemText)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                    dialog.dismiss()
+                }
+                // Показать диалоговое окно
+                builder.
+                create().show()
             }
         }
     }
